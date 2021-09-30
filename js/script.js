2036ea61ff4c3782cc0ab1b91f48d81ea6b5f4e6
AmreshSinha <theamresh.sinha@gmail.com>
2021-09-30 19:54:07 +0530

    Fix Copy and Generate Button

--- js/script.js
+++ js/script.js
@@ -12,4 +12,14 @@ function defaultPassword(){
         
     document.getElementById('pass').value = pass;
     console.log(pass);
+
+    document.getElementById("copyButtonIcon").className = "fas fa-copy me-4"
+    document.getElementById("copyButtonText").innerHTML = "Copy"
+}
+
+function copyButton(){
+    var copyText = document.getElementById("pass")
+    navigator.clipboard.writeText(copyText.value);
+    document.getElementById("copyButtonIcon").className = "fas fa-check me-4"
+    document.getElementById("copyButtonText").innerHTML = "Copied!"
 }
